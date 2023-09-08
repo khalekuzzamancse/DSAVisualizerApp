063bf8c8de6ee7670a26abec6f6092e8babcaf29
Khalekuzzaman <74848657+khalekuzzamancse@users.noreply.github.com>
2023-09-08 21:53:40 +0600

    Input Controll added

--- app/src/main/java/com/khalekuzzamanjustcse/dsavisulizer/DragableSetAbleComponent.kt
+++ app/src/main/java/com/khalekuzzamanjustcse/dsavisulizer/DragableSetAbleComponent.kt
@@ -7,15 +7,25 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
@@ -28,11 +38,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.LayoutCoordinates
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.positionInParent
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -41,17 +53,67 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
+
 @Preview
 @Composable
+fun Demo() {
+    DraggableElementPreview()
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
 fun DraggableElementPreview() {
-    DraggableElement()
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxSize()
+    ) {
+        var text by remember {
+            mutableStateOf("60 50 40 30 20 10")
+        }
+        var start by remember { mutableStateOf(false) }
+        var list by remember {
+            mutableStateOf(emptyList<Int>())
+        }
+
+        ElevatedCard(
+            shape = RectangleShape,
+            modifier =
+            Modifier.padding(4.dp).
+            border(width =1.dp, Color.Black)
+
+        ) {
+            TextField(
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+                value = text,
+                onValueChange = { text = it }
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+            Button(onClick = {
+                val elements = text.split("[,\\s]+".toRegex())
+                list = elements.mapNotNull { it.toIntOrNull() }
+                start=!start
+            }) {
+                Text("Visualize")
+            }
+
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+        if(start){
+            DraggableElement(list)
+        }
+
+    }
+
 }
 
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
-fun DraggableElement() {
-    val list = listOf(40, 60, 30, 20, 10, 50)
-    val cellWidth = 100.dp
+fun DraggableElement(list: List<Int>) {
+    val cellWidth = 50.dp
     val density = LocalDensity.current.density
     cellWidth.value * density
     var cellPosition by remember {
@@ -117,12 +179,12 @@ fun DraggableElement() {
     }
     val coroutineScope = rememberCoroutineScope()
     val swapCellElement: (Int, Int) -> Unit = { i, j ->
-            val iThCellElementRef = arrayCells[i].currentElementReference.value
-            val jThCellElementRef = arrayCells[j].currentElementReference.value
-            getCellCurrentElement(i).position.value = arrayCells[j].position.value
-            getCellCurrentElement(j).position.value = arrayCells[i].position.value
-            updateCurrentElement(i, jThCellElementRef ?: -1)
-            updateCurrentElement(j, iThCellElementRef ?: -1)
+        val iThCellElementRef = arrayCells[i].currentElementReference.value
+        val jThCellElementRef = arrayCells[j].currentElementReference.value
+        getCellCurrentElement(i).position.value = arrayCells[j].position.value
+        getCellCurrentElement(j).position.value = arrayCells[i].position.value
+        updateCurrentElement(i, jThCellElementRef ?: -1)
+        updateCurrentElement(j, iThCellElementRef ?: -1)
 
 //
         //new code with temporary variables ,fixed the bug later insha-allah
@@ -182,18 +244,19 @@ fun DraggableElement() {
                 }
             }
 
-            Box(modifier = Modifier
-                .size(cellWidth)
-                .border(color = Color.Black, width = 2.dp)
-                .onGloballyPositioned { cellPosition ->
-                    tempVariableCell.position.value = cellPosition.positionInParent()
-                })
+//            Box(modifier = Modifier
+//                .size(cellWidth)
+//                .border(color = Color.Black, width = 2.dp)
+//                .onGloballyPositioned { cellPosition ->
+//                    tempVariableCell.position.value = cellPosition.positionInParent()
+//                })
 
         }
 
 
         elements.forEachIndexed { index, element ->
             DraggableElement(
+                size=cellWidth,
                 label = "${element.value}",
                 currentOffset = elements[index].position.value,
                 onDragStart = {
